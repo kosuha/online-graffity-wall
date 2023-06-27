7ed124eb89de3b774d6351d3aaf9bdf7635ca4e1
Seonho Kim <okeydokekim@gmail.com>
2023-06-27 21:04:42 +0900

    .

--- client/src/store.ts
+++ client/src/store.ts
@@ -1,6 +1,6 @@
 import { writable } from 'svelte/store';
 import { io } from 'socket.io-client';
 
-const socket = io('http://ec2-100-26-112-62.compute-1.amazonaws.com/');
+const socket = io('http://ec2-100-26-112-62.compute-1.amazonaws.com/3000');
 
 export const socketStore = writable(socket);
